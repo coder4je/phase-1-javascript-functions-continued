2802388235eaf38009eeb80dfd8a3498f9e75fe7
Taemin Kim <taeminkimny@gmail.com>
2022-05-17 09:16:16 -0400

    commit tree ff9761009eed4523328afb0e9116f575cfa1bd38

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function mondayWork (toDo="go to the office") {
+    let plan = `This Monday, I will ${toDo}.`
+    return plan;
+}
+
+function saturdayFun(toDo2="roller-skate") {
+    return `This Saturday, I want to ${toDo2}!`
+} 
+
+
+
+function wrapAdjective(msg="special") {
+    return function cheerUp(msg2="*") {
+        const cheerUpFunction = `You are ${msg}${msg2}${msg}!`
+        return cheerUpFunction;
+    } 
+}
+
+
+wrapAdjective("%")("a dedicated programmer")
+
